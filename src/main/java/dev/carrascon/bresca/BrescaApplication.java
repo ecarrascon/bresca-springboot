5c40f706c928fa1352a521ac4f74b88f52c94a78
Eduardo Carrascon <carrascon.eduardo@gmail.com>
2023-04-28 09:11:06 +0200

    Starting app

---
+++ src/main/java/dev/carrascon/bresca/BrescaApplication.java
@@ -0,0 +1,13 @@
+package dev.carrascon.bresca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrescaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrescaApplication.class, args); //
+    }
+
+}
---
+++ src/test/java/dev/carrascon/bresca/BrescaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carrascon.bresca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrescaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
